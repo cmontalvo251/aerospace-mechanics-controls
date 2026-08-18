c82121cb58755396fb51d47aa399c30dfe00d10f
Carlos Montalvo <15804814+cmontalvo251@users.noreply.github.com>
2026-08-18 10:05:47 -0500

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -1,6 +1,33 @@
 var ptx_lunr_search_style = "textbook";
 var ptx_lunr_docs = [
 {
+  "id": "current-edition",
+  "level": "1",
+  "url": "current-edition.html",
+  "type": "Section",
+  "number": "1",
+  "title": "Current Edition",
+  "body": " Current Edition  This textbook was last updated on . The latest pdf can be found on Github while the most up to date web based version is on Github Pages .  A note on AI: Note that many sections have been written with the help of Github’s Copilot and even Google’s Gemini . Originially their were plans to properly cite each section that was written with the help of AI but the authors lost track given it’s seamless integration with VScode and Android smart phones as well as any browser at this point. As such, it is important to note that after September of 2025 it is safe to assume that most if not all sections were written with the help of AI. The authors are aware of the ethical implications of this issue however are rigorously check all sections and text to ensure that the content is correct and free of errors.  "
+},
+{
+  "id": "manuscript-changes",
+  "level": "1",
+  "url": "manuscript-changes.html",
+  "type": "Section",
+  "number": "2",
+  "title": "Manuscript Changes",
+  "body": " Manuscript Changes    June 10th, 2020 - Magnetic Field Model section updated to reflect the difference between the East North Vertical and the North East Down reference frames. The Figure showing the magnetic field for an example Low Earth Orbit has also been update. Also added this page for manuscript changes and the following pages that list where this file can be found.  December 10th, 2020 - Moved to public Github Repo separate from MATLAB  December 30th, 2020 - Moved papers.bib to parent directory Added datetime to title page. Added section headers to RC aircraft design. Wrote text all the way through the airfoil selection. Added a references section.  December 31st, 2020 - Finished RC aircraft section.  September 30th, 2021 - Renamed report to Aerospace Mechanics and Controls  December 21st, 2021 - Moved CubeSAT abstract to introduction on CubeSATs. Imported entire Aircraft Mechanics textbook into here  December 22nd, 2021 - Added a section on GNC design for CubeSats. Added acknowledgements section.  June 2nd, 2022 - Added some items to changes needed and fixed two references  July 30th, 2022 - Included a derivation of direct measurement of Euler Angles using an IMU.  July 31st, 2022 - Added GPS coordinate conversion to cartesian coodinates as well as heading angle and speed estimation from GPS.  October 27th, 2022 - Added computation of lat, lon, alttiude to ECI frame.  March 20th, 2024 - Added a Current Edition section above Manuscript Changes. Also added color to hyperlinks  May 7th, 2024 - Added a battery sizing section to the aircraft design chapter  July 20th, 2024 - separated sections into separate files...Also started adding the quacopter aerodynamic model  July 22nd, 2024 - Finished the quadcopter aerodynamics section and made a quick edit to the GNC aircraft PID control scheme  December 23rd, 2024 - Added a better description to the gravity model to explain the vector notation of the equations  January 6th, 2025 - A title change was performed for the GPS to Cartesian coordinate transform. Also made orbital elements its own section.  January 9th, 2025 - Edited the position of different sections so that it's ready for the linear controls section  May 13th, 2025 - Added drag equation to aerodynamics  September 7-8th, 2025 - Used Github's copilot on the Mobile App to create the first order example of a differential equation. Also used Gemini as well to create the second order example. I also added a citation for Gemini in the papers.bib file. I also edited the foreword to include a note on AI since from here on out I will use it to help write many sections of this manuscript.  September 10th, 2025 - Added the equations of motion for a second order mass spring damper system.  September 28th, 2025 - Moved the section on numerical integration to be before the LTI section since it makes more sense to have it there. Also added the second order solution showing underdamped, overdamped and critically damped solutions.  September 29th, 2025 - Added the time response section for first order systems and finished the derivation of first and second order systems.  September 30th, 2025 - added more section headers to the LTI section to make it easier to navigate.  October 10th, 2025 - Split the LTI Controls section into its own file and started writing the feedback control section.  October 11th, 2025 - Added the attitude dynamics of a satellite\/quadcopter section. Also rearranged the sections overall  October 12th, 2025 - Finished the rocket dynamics equations and started on the stability section.  October 13th, 2025 - Added the pitch dynamics of an aircraft  October 14th, 2025 - Finished the dynamics section of LTI systems and am working on the time response section now  October 15th, 2025 - Added the week by week schedule for the project based learning section  October 18th, 2025 - Added the code for simulating the mass spring damper system  October 19th, 2025 - Added stability sections for mass spring damper and the car velocity\/position  October 20th, 2025 - Added the time response and stability sections for the quadcopter\/satellite attitude dynamics  October 21st, 2025 - Split the lti section again so that's it's easier to read. Also finished the pitch dynamics of an aircraft.  October 22nd, 2025 - Added the rocket response and stability section.  October 24th, 2025 - Added the pendulum response and stability section  December 5th, 2025 - Fixed the aircraft dynamics equations as there was a sign error. Updated changes needed onto Github.  August 18th, 2026 - Started the conversion from LaTeX to PreTeXt.    "
+},
+{
+  "id": "future-changes",
+  "level": "1",
+  "url": "future-changes.html",
+  "type": "Section",
+  "number": "3",
+  "title": "Changes Needed and Future Project Ideas",
+  "body": " Changes Needed and Future Project Ideas  Needed changes and Future Project Ideas are now tracked on Github  "
+},
+{
   "id": "front-colophon",
   "level": "1",
   "url": "front-colophon.html",
@@ -10,22 +37,31 @@ var ptx_lunr_docs = [
   "body": "  "
 },
 {
-  "id": "sec-section-name",
+  "id": "conclusions",
   "level": "1",
-  "url": "sec-section-name.html",
-  "type": "Section",
-  "number": "1.1",
-  "title": "Section Title",
-  "body": " Section Title  Text of section.  "
+  "url": "conclusions.html",
+  "type": "Chapter",
+  "number": "1",
+  "title": "Conclusions",
+  "body": " Conclusions   Turns out I never wrote a conclusion section for this book. I guess I will get back to this later.   "
 },
 {
-  "id": "backmatter-2",
+  "id": "acknowledgements",
   "level": "1",
-  "url": "backmatter-2.html",
-  "type": "Colophon",
+  "url": "acknowledgements.html",
+  "type": "Chapter",
+  "number": "2",
+  "title": "Acknowledgements",
+  "body": " Acknowledgements   Carlos Montalvo would like to thank numerous students for their contribution to this document. They have been instrumental in making this textbook a reality and this textbook would not be where it is today without them. Those students are: Weston Barron, Colin Mcgee, Darcey D’Amato, Ruthie Hill, Drew Russ, William Sherman, Maxwell Cobar, Wei Min Patrick, Caroline Franklin, Andrew Givens, Aaron Godfrey, Nghia Huynh, Lisa Schibelius, and Brandon Troub.   "
+},
+{
+  "id": "backmatter-1",
+  "level": "1",
+  "url": "backmatter-1.html",
+  "type": "References",
   "number": "",
-  "title": "Colophon",
-  "body": " This book was authored in PreTeXt .  "
+  "title": "References",
+  "body": " References  GitHub. GitHub Copilot. https:\/\/copilot.github.com\/, 2021. Accessed on December 5, 2025.  Google DeepMind. Gemini: Googles multimodal ai model. https:\/\/gemini.google.com\/, 2023. Accessed on December 5, 2025.  Wikipedia. V-model. https:\/\/en.wikipedia.org\/wiki\/V-Model. Accessed December 21st, 2021.  Draw.io. JGraph Ltd, 2026, www.drawio.com\/. Accessed 29 Mar. 2026  Blender. Blender Foundation, 2026, www.blender.org\/. Accessed 29 Mar. 2026  Fritzing. Fritzing, 2026, fritzing.org\/. Accessed 29 Mar. 2026  SolidWorks. Dassault Systèmes, 2026, www.solidworks.com\/. Accessed 29 Mar. 2026  Autodesk. Autodesk, 2026, www.autodesk.com\/. Accessed 29 Mar. 2026  Cirkit Designer. Cirkit, 2026, https:\/\/app.cirkitdesigner.com\/. Accessed 29 Mar. 2026  Inkscape. Inkscape, 2026, www.inkscape.org\/. Accessed 29 Mar. 2026  CircuitPython. https:\/\/circuitpython.org\/, 2021. Accessed: 12-09-2021.  Carlos Montalvo, Lisa Schibelius, and Marine Leabeater. Project Based Engineering Instrumentation with CircuitPython. Github, 2023. https:\/\/cmontalvo251.github.io\/project-based-instrumentation\/.  Adafruit. Adafruit learn, 2021. Adafruit https:\/\/learn.adafruit.com\/.  Carlos Montalvo. Facility for aerial systems and technology github repository. cmontalvo251, 2022. https:\/\/github.com\/cmontalvo251  Adafruit. Adafruit, 2023. Adafruit https:\/\/adafruit.com\/.  Arduino. Arduino, 2023. https:\/\/www.arduino.cc\/.  Montalvo, Carlos. YouTube, 2026, www.youtube.com\/@CarlosMontalvo251.  Python Software Foundation. Python, 2026. https:\/\/www.python.org\/.  Aasumets, Aivar. Thonny: Python IDE for Beginners. 2026, thonny.org\/.  Spyder: The Scientific Python Development Environment. Spyder Project Contributors, 2026, www.spyder-ide.org\/.  Google Colaboratory. Google, 2026. https:\/\/colab.research.google.com\/.  Mu Editor. Mu, 2026. https:\/\/codewith.mu\/en\/.  GNU Octave. GNU, 2026. https:\/\/www.gnu.org\/software\/octave\/.  Google Sheets. Google, 2026. https:\/\/www.google.com\/sheets\/about\/.  "
 }
 ]
 
